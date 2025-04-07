280a61ee5a1b464f7ba3b3d7278a0e2e8f98b710
mjforgame <119397519+mjforgame@users.noreply.github.com>
2025-04-07 18:09:16 +0800

    add APP_CONFIG env

--- src/app/config/page.tsx
+++ src/app/config/page.tsx
@@ -22,7 +22,7 @@ export default async function Config({
 }) {
   const verify = Object.hasOwn(searchParams, "verify");
 
-  const appConfig = await getConfig("config.json");
+  const appConfig = process.env.APP_CONFIG ? process.env.APP_CONFIG : await getConfig("config.json");
 
   const { bgConfig, primaryColor } = transformConfig(appConfig);
 
